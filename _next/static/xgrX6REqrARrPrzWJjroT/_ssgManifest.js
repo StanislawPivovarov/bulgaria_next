ba7c2a7112f563705aa5f93604ec3e85e790eb00
Стас Пивоваров <stas.povovarov@headmade.pro>
2025-06-25 15:34:03 +0300

    Updates

---
+++ _next/static/xgrX6REqrARrPrzWJjroT/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[slug]","\u002F[slug]\u002F[slug_2]","\u002Fglossary","\u002Fportfolio","\u002Fsearch"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
